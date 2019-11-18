5d0d8751d09043d7f2e0039ec9c044b0768058fe
unknown <tateguerel@gmail.com>
2019-11-18 01:12:42 +0100

    collission detect successful

---
+++ Assets/Scripts/FeedScript.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedScript : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collided");
+    }
+
+
+}
+
